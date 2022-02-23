e6c865848ecdf2f277bb183f932b01074d8fcec6
osama-ameer <osamaameer15@gmail.com>
2022-02-23 10:00:08 +0500

    update product page

--- client/app/containers/ProductPage/index.js
+++ client/app/containers/ProductPage/index.js
@@ -128,9 +128,9 @@ class ProductPage extends React.PureComponent {
                         }}
                       />
                     </div>
-                    <div className='my-4 item-share'>
+                    {/* <div className='my-4 item-share'>
                       <SocialShare product={product} />
-                    </div>
+                    </div> */}
                     <div className='item-actions'>
                       {itemsInCart.includes(product._id) ? (
                         <Button
